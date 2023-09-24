348f857a7120f8a9912595b129257c85dcda9361
SkyZeroZx <73321943+SkyZeroZx@users.noreply.github.com>
2023-09-23 22:17:10 -0500

    (feat): Add Unit Test Auth Service

--- src/app/services/auth/auth.service.spec.ts
+++ src/app/services/auth/auth.service.spec.ts
@@ -1,6 +1,7 @@
 import { TestBed } from '@angular/core/testing';
 
 import { AuthService } from './auth.service';
+import { environment } from '../../../environments/environment';
 
 describe('AuthService', () => {
 	let service: AuthService;
@@ -12,5 +13,12 @@ describe('AuthService', () => {
 
 	it('should be created', () => {
 		expect(service).toBeTruthy();
+	});
+
+	it('should be return token', () => {
+		const tokenFromService = service.getToken();
+
+		expect(tokenFromService).toEqual(environment.authorId);
+		expect(tokenFromService).toBeDefined();
 	});
 });
